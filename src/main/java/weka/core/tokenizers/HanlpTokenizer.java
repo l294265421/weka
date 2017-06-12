16781eb4c9a7165d96b82b16581e445e3cca50ed
yuncong_li <yuncong_li@kingdee.com>
2017-06-12 16:45:10 +0800

    HanlpTokenizer，添加了支持词性选择的选项

--- src/main/java/weka/core/tokenizers/HanlpTokenizer.java
+++ src/main/java/weka/core/tokenizers/HanlpTokenizer.java
@@ -1,18 +1,64 @@
 package weka.core.tokenizers;
 
+import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Vector;
 
 import com.hankcs.hanlp.HanLP;
 import com.hankcs.hanlp.seg.common.Term;
 
+import weka.core.Option;
+import weka.core.Utils;
+
 public class HanlpTokenizer extends Tokenizer {
 
 	private static final long serialVersionUID = -8969384620055979993L;
+	
+	/**
+	 * 磁性前缀集合，通过,(英文逗号隔开)，all表示所有词性
+	 */
+	private String partOfSpeachs = "all";
+	
 	private String[] splitString;
 	private int currentPosition;
 	private int maxPosition;
 	
+	public String getPartOfSpeachs() {
+		return partOfSpeachs;
+	}
+
+	public void setPartOfSpeachs(String partOfSpeachs) {
+		this.partOfSpeachs = partOfSpeachs;
+	}
+
+	@Override
+	public Enumeration listOptions() {
+		Vector<Option> result = new Vector<>();
+		result.add(new Option("分词后保留的词性前缀", "partOfSpeachs", 1, "-p"));
+		return result.elements();
+	}
+
+	@Override
+	public String[] getOptions() {
+	    Vector<String>	result;
+	    result = new Vector<String>();
+	    result.add("-partOfSpeach");
+	    result.add(getPartOfSpeachs());
+	    return result.toArray(new String[result.size()]);
+	}
+
+	@Override
+	public void setOptions(String[] options) throws Exception {
+		String tmp = Utils.getOption("partOfSpeach", options);
+		if (tmp.length() == 0) {
+			setPartOfSpeachs("all");
+		} else {
+			setPartOfSpeachs(tmp);
+		}
+		super.setOptions(options);
+	}
+
 	@Override
 	public String getRevision() {
 		return "hanlp 1.3.2";
@@ -38,8 +84,8 @@ public class HanlpTokenizer extends Tokenizer {
 		List<Term> terms = HanLP.segment(s);
 		List<String> words = new LinkedList<>();
 		for(Term term : terms) {
-			if (term != null && (term.nature.startsWith('n') 
-					|| term.nature.startsWith('a') || term.nature.startsWith('v'))) {
+			if (term != null && term.word != null && term.word.length() != 0 
+					&& acceptByPartOfSpeach(term.nature.name())) {
 				words.add(term.word);
 			}
 		}
@@ -48,4 +94,19 @@ public class HanlpTokenizer extends Tokenizer {
 		currentPosition = 0;
 	}
 
+	private boolean acceptByPartOfSpeach(String partOfSpeach) {
+		if (partOfSpeach == null || partOfSpeach.length() == 0) {
+			return false;
+		}
+		if ("all".equals(this.partOfSpeachs)) {
+			return true;
+		}
+		String[] tmps = this.partOfSpeachs.split(",");
+		for (String tmp : tmps) {
+			if (partOfSpeach.startsWith(tmp)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
